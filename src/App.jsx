577852c92d987d2ff0fc3fa7d0f8b336de80de4d
brenoingwersen <brenoingwersen@gmail.com>
2024-02-25 19:15:29 -0300

    Add remaining functional components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import styles from './App.module.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+	// Current time
+	const [time, setTime] = useState(new Date());
+
+	// Creates an interval that updates the time veriable
+	// every 1000 ms
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setTime(new Date()); // Get current time
+		}, 1000);
+
+		return () => (clearInterval(intervalId));
+	}, []) // Call useEffect only on the first mount
+
+	function formatTime() {
+		const hours = time.getHours().toString().padStart(2, "0");
+		const minutes = time.getMinutes().toString().padStart(2, "0");
+		const seconds = time.getSeconds().toString().padStart(2, "0");
+		return `${hours}:${minutes}:${seconds}`
+	}
+
 	return (<>
 		<div className={styles["clock-container"]}>
 			<span className={styles["clock"]}>
-				00:00:00
+				{formatTime()}
 			</span>
 		</div>
 	</>);
